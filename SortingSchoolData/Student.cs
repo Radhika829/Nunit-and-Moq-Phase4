a8336d3713239e249ecf0267cde51bbddcf952af
Radhika <radhikasn06@gmail.com>
2024-02-20 08:08:59 +0530

    success

---
+++ SortingSchoolData/Student.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SortingSchoolData
+{
+    public class Student
+    {
+        private string _name;
+
+        public string Name
+        {
+            get { return _name; }
+            set { _name = value; }
+        }
+
+        private int _class;
+
+        public int Class
+        {
+            get { return _class; }
+            set { _class = value; }
+        }
+
+        private char _section;
+
+        public char Section
+        {
+            get { return _section; }
+            set { _section = value; }
+        }
+
+       
+
+        public bool Search(string s)
+        {
+            List<Student> students = new List<Student>();
+
+            students.Add(new Student { Name = "Ravi", Class = 10, Section = 'D' });
+            students.Add(new Student { Name = "Chethana", Class = 7, Section = 'B' });
+            students.Add(new Student { Name = "Sanjana", Class = 8, Section = 'C' });
+
+            var isfound = students.Find(d => d.Name == s);
+            return isfound != null;
+        }
+    }
+}
---
+++ SortingSchoolData/Teacher.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+
+namespace SortingSchoolData
+{
+    public class Teacher
+    {
+        private string _name;
+
+        public string Name
+        {
+            get { return _name; }
+            set { _name = value; }
+        }
+
+
+        private string _subject;
+
+        public string Subject
+        {
+            get { return _subject; }
+            set { _subject = value; }
+        }
+
+        public bool Search(string s)
+        {
+            List<Teacher> teachlist = new List<Teacher>();
+
+            teachlist.Add(new Teacher { Name = "Sakthi", Subject = "Maths" });
+            teachlist.Add(new Teacher { Name = "Ram", Subject = "English" });
+            teachlist.Add(new Teacher { Name = "Ganesh", Subject = "Science" });
+
+            var isfound = teachlist.Find(d => d.Name == s);
+            return isfound != null;
+        }
+
+    }
+}
